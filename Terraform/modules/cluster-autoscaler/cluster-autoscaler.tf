2b43f43c0f90c4821551003f0659bf6cdf8c9e4a
sugesh-cloudops <learndevopsallday@gmail.com>
2025-07-09 00:14:23 +0200

    hotfix

--- Terraform/modules/cluster-autoscaler/cluster-autoscaler.tf
+++ Terraform/modules/cluster-autoscaler/cluster-autoscaler.tf
@@ -11,9 +11,8 @@ resource "aws_iam_role" "cluster_autoscaler" {
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
-          "StringEquals" = {
-            "pods.eks.amazonaws.com/sa-name"      = "cluster-autoscaler",
-            "pods.eks.amazonaws.com/sa-namespace" = "kube-system"
+          "StringEquals" : {
+            "pods.eks.amazonaws.com/assume-role" : "true"
           }
         }
       }
