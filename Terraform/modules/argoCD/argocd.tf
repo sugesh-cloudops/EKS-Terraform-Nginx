3b873db7e2ea35efea498090344ee797448fc223
sugesh-cloudops <learndevopsallday@gmail.com>
2025-07-21 12:52:20 +0200

    argocd port update

--- Terraform/modules/argoCD/argocd.tf
+++ Terraform/modules/argoCD/argocd.tf
@@ -10,6 +10,14 @@ resource "helm_release" "argocd" {
         name  = "server.service.type"
         value = "LoadBalancer"
     }
-    
+    set {
+        name  = "server.service.ports.https.port"
+        value = "443"
+    }
+
+    set {
+    name  = "server.service.ports.https.targetPort"
+    value = "8080"
+    }
   
 }
